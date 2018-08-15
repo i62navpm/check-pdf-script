36a7c87945aacdaf4f94d04c501ecba5d5e9124e
m.navarro <m.navarro@payvision.com>
2018-08-15 13:08:42 +0200

    Added winston configuration

---
+++ src/winston.js
@@ -0,0 +1,26 @@
+const { createLogger, format, transports } = require('winston')
+const { combine, timestamp, json, colorize } = format
+
+const options = {
+  file: {
+    level: 'info',
+    filename: `./log/app.log`,
+    format: combine(timestamp(), json()),
+    handleExceptions: true,
+  },
+  console: {
+    level: 'debug',
+    handleExceptions: true,
+    format: format.combine(colorize(), format.simple()),
+  },
+}
+
+let logger = createLogger({
+  transports: [
+    new transports.File(options.file),
+    new transports.Console(options.console),
+  ],
+  exitOnError: false,
+})
+
+module.exports = logger
